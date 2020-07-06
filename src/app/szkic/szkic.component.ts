4ff40fdc954eddc0e2701cf8adf2f4b1616e840b
Zagii <zagawus@interia.pl>
2020-07-06 15:52:31 +0200

    2 files modified

--- src/app/szkic/szkic.component.ts
+++ src/app/szkic/szkic.component.ts
@@ -7,32 +7,35 @@ import {CdkDragDrop, moveItemInArray,copyArrayItem,transferArrayItem} from '@ang
   styleUrls: ['./szkic.component.css']
 })
 export class SzkicComponent implements OnInit {
- todo = [
-    'Get to work',
-    'Pick up groceries',
-    'Go home',
-    'Fall asleep'
+  
+  constructor() { }
+
+  ngOnInit() {
+  }
+ narzedzia = [
+    'Start',
+    'podlej',
+    'czekaj',
+    'nie lej',
+    'stop'
   ];
 
-  done = [
-    'Get up',
-    'Brush teeth',
-    'Take a shower',
-    'Check e-mail',
-    'Walk dog'
+  sekwencja = [
+  
   ];
 
   drop(event: CdkDragDrop<string[]>) {
     if (event.previousContainer === event.container) {
       moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
     } else {
-      /*transferArrayItem(event.previousContainer.data,
+    /*  transferArrayItem(event.previousContainer.data,
                         event.container.data,
                         event.previousIndex,
-                        event.currentIndex);*/
-        copyArrayItem(event.previousContainer.data,
+                        event.currentIndex);
+      */  copyArrayItem(event.previousContainer.data,
         event.container.data,
         event.previousIndex,event.currentIndex);
+    
     }
   }
 }
